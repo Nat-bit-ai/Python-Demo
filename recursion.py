a47c3d36959d8abcad1bc2f81097cc8594d12966
Natnael Zerihun <nathyzer21@gmail.com>
2026-06-27 13:59:54 +0300

    fibonacci recursion

--- recursion.py
+++ recursion.py
@@ -19,3 +19,11 @@ def fac(n):
         return 1
     return n*fac(n-1)
 print(fac(5))
+#fibonacci
+def fib(n):
+    if n == 0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1)+fib(n-2)
+print(fib(5))

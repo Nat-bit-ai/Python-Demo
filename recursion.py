a01b8497899f15e024cca2d32d7beb04e8864edf
Natnael Zerihun <nathyzer21@gmail.com>
2026-06-27 14:19:09 +0300

    power 2 3 4

--- recursion.py
+++ recursion.py
@@ -33,3 +33,39 @@ def GCD(a,b):
         return a
     return GCD(b, a%b)
 print(GCD(48,18))
+#power of 2
+def pow2(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n % 2 != 0:
+        return False
+    return pow2(n // 2)
+
+print(pow2(8))
+#power of 3
+def pow3(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n % 3 != 0:
+        return False
+    return pow3(n // 3)
+
+print(pow3(8))
+
+#power of 4
+def pow4(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n % 4 != 0:
+        return False
+    return pow4(n // 4)
+
+print(pow4(8))
+
+

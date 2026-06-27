5ea7775d968b7d90741448a3d0cba94876f1ffab
Natnael Zerihun <nathyzer21@gmail.com>
2026-06-27 13:42:59 +0300

    sum of digits practice in base k

--- recursion.py
+++ recursion.py
@@ -1,1 +1,7 @@
-git
+# sum of digits base k
+def sumOfDigits( n, k):
+    assert n>=0 and int(n) == n , 'integer input only'
+    if n == 0:
+        return 0
+    return n%k + sumOfDigits(n//k , k)
+print(sumOfDigits(7,2))

bfacca45fb61ff23b82732d03e65267034a951da
Natnael Zerihun <nathyzer21@gmail.com>
2026-07-01 12:39:08 +0300

    reverse method

--- array demo.py
+++ array demo.py
@@ -3,42 +3,9 @@
 from array import *
 arr1 = array('i', [1,2,3,4,5,6])
 
-def traverse(arr):
-    for i in arr:
-        print(i)
-traverse(arr1)
 
- #accessing elements through index
 
-def access(arr , value):
-    for i in arr:
-        if i == value:
-            print(arr.index(value))
-access(arr1 , 3)
+#reverse method
 
-#appending value
-
-def appends(arr , val):
-    return arr.append(val)
-
-appends(arr1,7)
-
-# insertion
-
-def insertion(arr, value, index):
-    return arr.insert(index, value)
-
-insertion(arr1 ,9 ,2)
-
-# extending
-
-def extending(arr,arr2):
-    return arr.extend(arr2)
-
-extending(arr1,[9,10,11])
-print(arr1)
-
-#pop method
-
-arr1.pop()
+arr1.reverse()
 print(arr1)
